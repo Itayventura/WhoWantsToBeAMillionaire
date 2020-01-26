c7a7fc5638e1b7839af3760da7f1e61a7eaaebc6
adieldar1 <adieldar1995@gmail.com>
2020-01-26 21:17:27 +0200

    changed repositories

---
+++ SRC/APPLICATION-SOURCE-CODE/app.py
@@ -0,0 +1,285 @@
+import random
+from flask import Flask, render_template, jsonify, request, session, redirect
+from database import Database
+from constants import *
+
+app = Flask(__name__)
+app.secret_key = 't3mp_k3y'
+service_port = 40004
+db = Database()
+
+
+def generate_question():
+    """ This function chooses a random question and calls the method that generates the question's data.
+    :return: The question's data.
+    """
+    questions_list = [artist_with_album_released_in_specific_decade_with_love_song,
+                      avg_tracks_for_artist_albums,
+                      artist_with_more_albums_than_avg,
+                      movie_with_most_played_tracks_in_genre,
+                      artist_with_mainly_tracks_from_specific_genre,
+                      highest_rated_artist_without_movie_tracks,
+                      sentence_to_fill_with_missing_word,
+                      the_most_rated_artist,
+                      first_released_album_out_of_four,
+                      track_in_movie,
+                      movie_without_track,
+                      track_of_specific_artist,
+                      year_of_birth_of_specific_artist,
+                      song_that_contains_a_word]
+    random.shuffle(questions_list)
+    return questions_list[0]()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        session['number'] = 0
+        session['question_number'] = 1
+        session['target'] = 70
+        data, session['correct'] = generate_question()
+        return render_template('index.html', **data)
+    else:
+        req_data = request.get_data().decode('utf-8')
+        last_correct = session['correct']
+        session['question_number'] += 1
+        data, session['correct'] = generate_question()
+        data['correct'] = 'false'
+        data['win'] = 'false'
+        if last_correct == req_data:
+            data['correct'] = 'true'
+            session['number'] += 1
+            if session['number'] == session['target']:
+                data['win'] = 'true'
+            return jsonify(**data)
+        return jsonify(**data)
+
+
+def shuffle_answers(wrong_answers, correct_answer):
+    """ This function shuffles a question's four answers.
+    :param wrong_answers: (list) A list of three wrong answers.
+    :param correct_answer: (str) The correct answer.
+    :return: 1. (dict) A dictionary of the shuffled answers.
+             2. (str) The key of the correct answer.
+    """
+    letters = ['a', 'b', 'c', 'd']
+    random.shuffle(letters)
+    all_answers = wrong_answers + [correct_answer]
+    answers = [("answer_"+letter, str(ans)) for letter, ans in zip(letters, all_answers)]
+    return dict(answers), "answer_" + letters[len(letters) - 1]
+
+
+def artist_with_more_albums_than_avg():
+    """ This function creates the data for the question:
+        'Which artist has more albums than the average?'
+    :return: 1. (dict) The question data.
+             2. (str) The key of the correct answer in data dict.
+    """
+    question = QUESTION_ARTIST_WITH_MORE_ALBUMS_THAN_AVG
+    correct_answer, wrong_answers = db.get_artist_with_more_albums_than_avg()
+    data, correct_answer_key = shuffle_answers(wrong_answers, correct_answer)
+    data['win'] = 'false'
+    data['correct'] = 'true'
+    data['question'] = question
+    return data, correct_answer_key
+
+
+def avg_tracks_for_artist_albums():
+    """ This function creates the data for the question:
+        'What is the average tracks number in <RANDOM_ARTIST>'s albums?'
+    :return: 1. (dict) The question data.
+             2. (str) The key of the correct answer in data dict.
+    """
+    correct_answer, wrong_answers, artist_name = db.get_avg_tracks_for_artist_albums()
+    question = QUESTION_AVG_TRACKS_IN_ALBUM_FOR_ARTIST.format(artist=artist_name)
+    data, correct_answer_key = shuffle_answers(wrong_answers, correct_answer)
+    data['win'] = 'false'
+    data['correct'] = 'true'
+    data['question'] = question
+    return data, correct_answer_key
+
+
+def movie_with_most_played_tracks_in_genre():
+    """ This function creates the data for the question:
+        'What is the movie with the most played tracks from genre <RANDOM_GENRE>?'
+    :return: 1. (dict) The question data.
+             2. (str) The key of the correct answer in data dict.
+    """
+    correct_answer, wrong_answers, genre_name = db.get_movie_with_most_played_tracks_in_genre()
+    question = QUESTION_MOVIE_WITH_MOST_PLAYED_TRACKS_FROM_GENRE.format(genre=genre_name)
+    data, correct_answer_key = shuffle_answers(wrong_answers, correct_answer)
+    data['win'] = 'false'
+    data['correct'] = 'true'
+    data['question'] = question
+    return data, correct_answer_key
+
+
+def artist_with_mainly_tracks_from_specific_genre():
+    """ This function creates the data for the question:
+        'To which of the following artists most of their tracks are from genre <RANDOM_GENRE>?'
+    :return: 1. (dict) The question data.
+             2. (str) The key of the correct answer in data dict.
+    """
+    correct_answer, wrong_answers, genre_name = db.get_artist_with_mainly_tracks_from_specific_genre()
+    question = QUESTION_ARTIST_WITH_MAINLY_TRACKS_FROM_SPECIFIC_GENRE.format(genre=genre_name)
+    data, correct_answer_key = shuffle_answers(wrong_answers, correct_answer)
+    data['win'] = 'false'
+    data['correct'] = 'true'
+    data['question'] = question
+    return data, correct_answer_key
+
+
+def artist_with_album_released_in_specific_decade_with_love_song():
+    """ This function creates the data for the question:
+        'Which of the following artists has an album released in the <RANDOM_DECADE>'s
+         that has a track which contains the word 'love'?'
+    :return: 1. (dict) The question data.
+             2. (str) The key of the correct answer in data dict.
+    """
+    correct_answer, wrong_answers, decade = db.get_artist_with_album_released_in_specific_decade_with_love_song()
+    question = QUESTION_ARTIST_WITH_ALBUM_WITH_LOVE_SONG.format(decade=str(decade))
+    data, correct_answer_key = shuffle_answers(wrong_answers, correct_answer)
+    data['win'] = 'false'
+    data['correct'] = 'true'
+    data['question'] = question
+    return data, correct_answer_key
+
+
+def highest_rated_artist_without_movie_tracks():
+    """ This function creates the data for the question:
+        'Which of the following is the highest rated artist that his tracks have not been played in any movie?'
+    :return: 1. (dict) The question data.
+             2. (str) The key of the correct answer in data dict.
+    """
+    question = QUESTION_HIGHEST_RATED_ARTIST_WITHOUT_MOVIE_TRACKS
+    correct_answer, wrong_answers = db.get_highest_rated_artist_without_movie_tracks()
+    data, correct_answer_key = shuffle_answers(wrong_answers, correct_answer)
+    data['win'] = 'false'
+    data['correct'] = 'true'
+    data['question'] = question
+    return data, correct_answer_key
+
+
+def sentence_to_fill_with_missing_word():
+    """ This function creates the data for the question:
+        'Complete the following line from <RANDOM TRACK> by <ARTIST>: <RANDOM_SENTENCE_WITH_MISSING_WORD>'
+    :return: 1. (dict) The question data.
+             2. (str) The key of the correct answer in data dict.
+    """
+    correct_answer, wrong_answers, sentence, track_name, artist_name = db.get_sentence_to_fill_with_missing_word()
+    question = QUESTION_FILL_THE_MISSING_WORD.format(track=track_name, artist=artist_name, sentence=sentence)
+    data, correct_answer_key = shuffle_answers(wrong_answers, correct_answer)
+    data['win'] = 'false'
+    data['correct'] = 'true'
+    data['question'] = question
+    return data, correct_answer_key
+
+
+def the_most_rated_artist():
+    """ This function creates the data for the question:
+        'Which of the following is the most highly rated artist?'
+    :return: 1. (dict) The question data.
+             2. (str) The key of the correct answer in data dict.
+    """
+    correct_answer, wrong_answers = db.get_the_most_rated_artist()
+    question = QUESTION_MOST_RATED_ARTIST
+    data, correct_answer_key = shuffle_answers(wrong_answers, correct_answer)
+    data['win'] = 'false'
+    data['correct'] = 'true'
+    data['question'] = question
+    return data, correct_answer_key
+
+
+def first_released_album_out_of_four():
+    """ This function creates the data for the question:
+        'Which of the following albums was the first to be released?'
+    :return: 1. (dict) The question data.
+             2. (str) The key of the correct answer in data dict.
+    """
+    correct_answer, wrong_answers = db.get_first_released_album_out_of_four()
+    question = QUESTION_FIRST_RELEASED_ALBUM
+    data, correct_answer_key = shuffle_answers(wrong_answers, correct_answer)
+    data['win'] = 'false'
+    data['correct'] = 'true'
+    data['question'] = question
+    return data, correct_answer_key
+
+
+def track_in_movie():
+    """ This function creates the data for the question:
+        'What track is played in the movie <RANDOM_MOVIE>?'
+    :return: 1. (dict) The question data.
+             2. (str) The key of the correct answer in data dict.
+    """
+    correct_answer, wrong_answers, movie_name = db.get_track_in_movie()
+    question = QUESTION_TRACK_IN_SPECIFIC_MOVIE.format(movie=movie_name)
+    data, correct_answer_key = shuffle_answers(wrong_answers, correct_answer)
+    data['win'] = 'false'
+    data['correct'] = 'true'
+    data['question'] = question
+    return data, correct_answer_key
+
+
+def movie_without_track():
+    """ This function creates the data for the question:
+        'In which movie the track <TRACK_NAME> is not played?'
+    :return: 1. (dict) The question data.
+             2. (str) The key of the correct answer in data dict.
+    """
+    correct_answer, wrong_answers, track_name = db.get_movie_without_track()
+    question = QUESTION_MOVIE_WITHOUT_SPECIFIC_TRACK.format(track=track_name)
+    data, correct_answer_key = shuffle_answers(wrong_answers, correct_answer)
+    data['win'] = 'false'
+    data['correct'] = 'true'
+    data['question'] = question
+    return data, correct_answer_key
+
+
+def track_of_specific_artist():
+    """ This function creates the data for the question:
+        'Which of the following tracks is played by the artist <RANDOM_ARTIST>?'
+    :return: 1. (dict) The question data.
+             2. (str) The key of the correct answer in data dict.
+    """
+    correct_answer, wrong_answers, artist_name = db.get_track_of_specific_artist()
+    question = QUESTION_TRACK_PLAYED_BY_SPECIFIC_ARTIST.format(artist=artist_name)
+    data, correct_answer_key = shuffle_answers(wrong_answers, correct_answer)
+    data['win'] = 'false'
+    data['correct'] = 'true'
+    data['question'] = question
+    return data, correct_answer_key
+
+
+def year_of_birth_of_specific_artist():
+    """ This function creates the data for the question:
+        'What is the birth date of <RANDOM_ARTIST>?'
+    :return: 1. (dict) The question data.
+             2. (str) The key of the correct answer in data dict.
+    """
+    correct_answer, wrong_answers, artist_name = db.get_year_of_birth_of_specific_artist()
+    question = QUESTION_SPECIFIC_ARTIST_DATE_OF_BIRTH.format(artist=artist_name)
+    data, correct_answer_key = shuffle_answers(wrong_answers, correct_answer)
+    data['win'] = 'false'
+    data['correct'] = 'true'
+    data['question'] = question
+    return data, correct_answer_key
+
+
+def song_that_contains_a_word():
+    """ This function creates the data for the question:
+        'Which of the following songs contains the word <RANDOM_WORD>?'
+    :return: 1. (dict) The question data.
+             2. (str) The key of the correct answer in data dict.
+    """
+    correct_answer, wrong_answers, word_in_song = db.get_song_that_contain_a_word_from_list_of_words()
+    question = QUESTION_SONG_CONTAINS_WORDS.format(word=word_in_song)
+    data, correct_answer_key = shuffle_answers(wrong_answers, correct_answer)
+    data['win'] = 'false'
+    data['correct'] = 'true'
+    data['question'] = question
+    return data, correct_answer_key
+
+
+if __name__ == "__main__":
+    app.run(port=service_port, host="0.0.0.0", debug=False)
+
---
+++ SRC/APPLICATION-SOURCE-CODE/sql_queries.py
@@ -0,0 +1,337 @@
+'''------------------------COMPLEX QUERIES-------------------------'''
+
+get_artist_with_more_albums_than_avg = """
+(SELECT Artists.artist_name
+     FROM Artists,
+          Albums
+     WHERE Albums.artist_id = Artists.artist_id
+     GROUP BY Artists.artist_id
+     HAVING COUNT(DISTINCT Albums.album_id) >
+         (SELECT AVG(nested.`count`)
+          FROM
+              (SELECT COUNT(DISTINCT album_id) AS `count`
+               FROM Albums
+               GROUP BY artist_id) AS nested)
+     ORDER BY RAND()
+     LIMIT 1)
+UNION
+    (SELECT Artists.artist_name
+     FROM Artists,
+          Albums
+     WHERE Albums.artist_id = Artists.artist_id
+     GROUP BY Artists.artist_id
+     HAVING COUNT(DISTINCT Albums.album_id) <=
+         (SELECT AVG(nested.`count`)
+          FROM
+              (SELECT COUNT(DISTINCT album_id) AS `count`
+               FROM Albums
+               GROUP BY artist_id) AS nested)
+     ORDER BY RAND()
+     LIMIT 3)
+"""
+
+get_avg_tracks_for_artist_albums = """
+SELECT AVG(tracks_count.num),
+       tracks_count.artist_name
+FROM
+    (SELECT COUNT(AlbumTracks.track_id) AS num,
+            rand_artist.artist_name AS artist_name
+     FROM
+         (SELECT Artists.artist_id,
+                 artist_name
+          FROM Artists, Albums
+          WHERE Artists.artist_id = Albums.artist_id
+          ORDER BY RAND()
+          LIMIT 1) AS rand_artist
+          LEFT JOIN Albums ON rand_artist.artist_id = Albums.artist_id
+          LEFT JOIN AlbumTracks ON Albums.album_id = AlbumTracks.album_id
+     GROUP BY Albums.album_id, artist_name) AS tracks_count
+GROUP BY tracks_count.artist_name
+"""
+
+get_movie_with_most_played_tracks_in_genre = """
+SELECT top_movie.movie_name,
+       Movies.movie_name,
+       top_movie.genre_name
+FROM
+    (SELECT Movies.movie_name,
+            Movies.movie_id,
+            rand_genre.genre_name
+     FROM
+         (SELECT genre_id AS genre_id,
+                 genre_name AS genre_name
+          FROM Genres
+          ORDER BY RAND()
+          LIMIT 1) AS rand_genre,
+          MovieTracks,
+          Tracks,
+          TracksGenres,
+          Movies
+     WHERE MovieTracks.track_id = Tracks.track_id
+         AND Tracks.track_id = TracksGenres.track_id
+         AND rand_genre.genre_id = TracksGenres.genre_id
+         AND Movies.movie_id = MovieTracks.movie_id
+     GROUP BY MovieTracks.movie_id,
+              rand_genre.genre_id
+     ORDER BY COUNT(MovieTracks.track_id) DESC
+     LIMIT 1) AS top_movie,
+     Movies
+WHERE Movies.movie_id != top_movie.movie_id
+ORDER BY RAND()
+LIMIT 3
+"""
+
+get_random_genre = """
+SELECT * 
+FROM Genres 
+ORDER BY RAND() 
+LIMIT 1
+"""
+
+get_artist_with_mainly_tracks_from_specific_genre = """
+        (SELECT artist_name
+     FROM Artists
+     WHERE EXISTS
+             (SELECT DISTINCT 1
+              FROM
+                  (SELECT Tracks.artist_id,
+                          COUNT(Tracks.track_id) AS num
+                   FROM TracksGenres,
+                        Tracks
+                   WHERE Tracks.track_id = TracksGenres.track_id
+                       AND TracksGenres.genre_id = '%s'
+                   GROUP BY Tracks.artist_id) AS total_pop_count_for_artist,
+
+                  (SELECT Tracks.artist_id,
+                          COUNT(Tracks.track_id) AS num
+                   FROM Tracks
+                   GROUP BY Tracks.artist_id) AS total_tracks_count_for_artist
+              WHERE Artists.artist_id = total_pop_count_for_artist.artist_id
+                  AND total_pop_count_for_artist.artist_id = total_tracks_count_for_artist.artist_id
+                  AND total_pop_count_for_artist.num > 0.5 * total_tracks_count_for_artist.num)
+     ORDER BY RAND()
+     LIMIT 1)
+UNION
+    (SELECT artist_name
+     FROM Artists
+     WHERE NOT EXISTS
+             (SELECT DISTINCT 1
+              FROM
+                  (SELECT Tracks.artist_id,
+                          COUNT(Tracks.track_id) AS num
+                   FROM TracksGenres, Tracks
+                   WHERE Tracks.track_id = TracksGenres.track_id
+                       AND TracksGenres.genre_id = '%s'
+                   GROUP BY Tracks.artist_id) AS total_pop_count_for_artist,
+
+                  (SELECT Tracks.artist_id,
+                          COUNT(Tracks.track_id) AS num
+                   FROM Tracks
+                   GROUP BY Tracks.artist_id) AS total_tracks_count_for_artist
+              WHERE Artists.artist_id = total_pop_count_for_artist.artist_id
+                  AND total_pop_count_for_artist.artist_id = total_tracks_count_for_artist.artist_id
+                  AND total_pop_count_for_artist.num > 0.5 * total_tracks_count_for_artist.num)
+     ORDER BY RAND()
+     LIMIT 3)
+"""
+
+get_artist_with_album_released_in_specific_decade_with_love_song = """
+    (SELECT Artists.artist_name
+     FROM Artists
+     WHERE artist_id = ANY
+             (SELECT Albums.artist_id
+              FROM Albums
+              WHERE Albums.release_date >= %s
+                  AND Albums.release_date < %s
+                  AND Albums.album_id = ANY
+                      (SELECT AlbumTracks.album_id
+                       FROM AlbumTracks,
+                            Tracks
+                       WHERE AlbumTracks.track_id = Tracks.track_id
+                           AND MATCH (lyrics) AGAINST ('Love' IN NATURAL LANGUAGE MODE)))
+     ORDER BY RAND()
+     LIMIT 1)
+UNION
+    (SELECT Artists.artist_name
+     FROM Artists
+     WHERE artist_id = ANY
+             (SELECT Albums.artist_id
+              FROM Albums
+              WHERE Albums.release_date >= %s
+                  AND Albums.release_date < %s
+                  AND Albums.album_id != ANY
+                      (SELECT AlbumTracks.album_id
+                       FROM AlbumTracks,
+                            Tracks
+                       WHERE AlbumTracks.track_id = Tracks.track_id
+                           AND MATCH (lyrics) AGAINST ('Love' IN NATURAL LANGUAGE MODE)))
+     ORDER BY RAND()
+     LIMIT 3)
+"""
+
+get_highest_rated_artist_without_movie_tracks = """
+SELECT * 
+FROM   (SELECT * 
+        FROM   (SELECT artist_rating, 
+                       (SELECT artist_name 
+                        FROM   Artists 
+                        WHERE  NOT EXISTS (SELECT 1 
+                                           FROM   MovieTracks, 
+                                                  Tracks 
+                                           WHERE  Artists.artist_id = 
+                                                  Tracks.artist_id 
+                                                  AND MovieTracks.track_id = 
+                                                      Tracks.track_id) 
+                               AND Artists.artist_rating = ratings.artist_rating 
+                        ORDER  BY rand() 
+                        LIMIT  1) AS artist_name 
+                FROM   (SELECT DISTINCT artist_rating 
+                        FROM   Artists) AS ratings) AS random_artists 
+        WHERE  artist_name IS NOT NULL 
+        ORDER  BY RAND() 
+        LIMIT  4) AS four_artists 
+ORDER  BY artist_rating DESC 
+"""
+
+'''------------------------SIMPLE QUERIES-------------------------'''
+
+get_random_track_lyrics = """
+SELECT Tracks.track_name, Artists.artist_name, Tracks.lyrics
+FROM Tracks, Artists
+WHERE Tracks.lyrics IS NOT NULL
+AND Tracks.artist_id = Artists.artist_id
+ORDER BY RAND()
+LIMIT 1
+"""
+
+get_the_most_rated_artist = """
+SELECT correct_artist.artist_name,
+       Artists.artist_name
+FROM
+    (SELECT Artists.artist_name,
+            Artists.artist_rating
+     FROM Artists
+     ORDER BY RAND()
+     LIMIT 1) AS correct_artist,
+     Artists
+WHERE Artists.artist_rating < correct_artist.artist_rating
+ORDER BY RAND()
+LIMIT 3
+"""
+
+get_first_released_album_out_of_four = """
+SELECT correct_album.album_name,
+       Albums.album_name
+FROM
+    (SELECT Albums.album_name,
+            Albums.release_date
+     FROM Albums
+     ORDER BY RAND()
+     LIMIT 1) AS correct_album,
+     Albums
+WHERE Albums.release_date > correct_album.release_date
+ORDER BY RAND()
+LIMIT 3
+"""
+
+get_track_in_movie = """
+SELECT answer.movie_name,
+       answer.track_name,
+       Tracks.track_name
+FROM
+    (SELECT Tracks.track_name,
+            Movies.movie_name,
+            Movies.movie_id
+     FROM Tracks,
+          MovieTracks,
+          Movies
+     WHERE Tracks.track_id = MovieTracks.track_id
+         AND MovieTracks.movie_id = Movies.movie_id
+     ORDER BY RAND()
+     LIMIT 1) AS answer,
+     Tracks,
+     MovieTracks
+WHERE Tracks.track_id = MovieTracks.track_id
+    AND MovieTracks.movie_id != answer.movie_id
+ORDER BY RAND()
+LIMIT 3
+"""
+
+get_movie_without_track = """
+SELECT Tracks.track_name,
+       answer.movie_name,
+       Movies.movie_name
+FROM
+    (SELECT played_track.track_id,
+            Movies.movie_name
+     FROM
+         (SELECT MovieTracks.track_id
+          FROM MovieTracks
+          GROUP BY MovieTracks.track_id
+          HAVING COUNT(*) >= 3
+          ORDER BY RAND()
+          LIMIT 1) played_track,
+          MovieTracks,
+          Movies
+     WHERE MovieTracks.movie_id = Movies.movie_id
+         AND MovieTracks.track_id != played_track.track_id
+     ORDER BY RAND()
+     LIMIT 1) AS answer,
+     Movies,
+     MovieTracks,
+     Tracks
+WHERE MovieTracks.track_id = answer.track_id
+    AND Movies.movie_id = MovieTracks.movie_id
+    AND Tracks.track_id = answer.track_id
+ORDER BY RAND()
+LIMIT 3
+"""
+
+get_track_of_specific_artist = """
+SELECT answer.track_name,
+       answer.artist_name,
+       Tracks.track_name
+FROM
+    (SELECT Tracks.track_name,
+            Tracks.artist_id,
+            Artists.artist_name
+     FROM Tracks,
+          Artists
+     WHERE Artists.artist_id = Tracks.artist_id
+     ORDER BY RAND()
+     LIMIT 1) answer,
+     Tracks
+WHERE Tracks.artist_id != answer.artist_id
+ORDER BY RAND()
+LIMIT 3
+"""
+
+get_year_of_birth_of_specific_artist = """
+SELECT Artists.artist_name,
+       Artists.start_date
+FROM Artists
+WHERE Artists.artist_type = 'Person'
+ORDER BY RAND()
+LIMIT 1
+"""
+
+'''------------------------full text query-------------------------'''
+
+get_song_that_contain_a_word_from_list_of_words = """
+(SELECT track_name
+FROM Tracks
+WHERE MATCH (lyrics) AGAINST ('%s' IN NATURAL LANGUAGE MODE)
+ORDER BY RAND()
+LIMIT 1)
+
+UNION 
+
+(SELECT track_name
+FROM Tracks
+WHERE track_name
+  not in (SELECT track_name
+            FROM Tracks
+            WHERE MATCH (lyrics) AGAINST ('%s' IN NATURAL LANGUAGE MODE))
+ORDER BY RAND()
+LIMIT 3)
+"""
